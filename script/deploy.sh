84574b2bd6eed2c434b25632de57b33b11154e42
Stephen James <kon1517@kroger.com>
2016-06-17 16:22:50 -0400

    Fixing deploy script.

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-  ./build.sh
+  ./script/build.sh
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
@@ -20,15 +20,15 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into out/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+# Clone the existing gh-pages for this repo into dist/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deploy)
 git clone $REPO dist
 cd dist
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-rm -rf dist/**/* || exit 0
+rm -rf dist/* || exit 0
 
 # Run our compile script
 doCompile
